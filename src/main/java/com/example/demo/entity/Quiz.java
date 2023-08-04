f7b75b9fc78b3f985ef284e25e998d590738d83c
HiranoHayata <yamatotakeru002@gmail.com>
2023-08-04 13:03:10 +0900

    sss

--- src/main/java/com/example/demo/entity/Quiz.java
+++ src/main/java/com/example/demo/entity/Quiz.java
@@ -1,5 +1,24 @@
 package com.example.demo.entity;
 
-public class Quiz {
+import org.springframework.data.annotation.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/** ruizテーブル用:Entity */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz{
+	/** 識別ID */
+	@Id
+	private Integer id;
+	/** クイズの内容 */
+	private String question;
+	/** クイズの解答 */
+	private Boolean answer;
+	/** 作成者 */
+	private String author;
 }
+
