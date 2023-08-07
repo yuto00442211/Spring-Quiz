577190245d5f57a9e893b8c3117bcca231d95c48
33sammy <kennysammi@gmail.com>
2023-08-07 11:30:35 +0900

    jdbc

--- src/main/java/com/example/demo/repository/QuizRepository.java
+++ src/main/java/com/example/demo/repository/QuizRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.entity.Quiz;
